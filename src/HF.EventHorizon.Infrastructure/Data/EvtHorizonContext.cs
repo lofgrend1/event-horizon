3c5b00ddcb0ddbafd1e2930f098d70fb8eeeaf31
Dustin Lofgren <dustin.lofgren@gmail.com>
2024-12-13 12:55:20 -0500

    forgot to add dbcontext stuff so i've added that stuff

--- src/HF.EventHorizon.Infrastructure/Data/EvtHorizonContext.cs
+++ src/HF.EventHorizon.Infrastructure/Data/EvtHorizonContext.cs
@@ -1,13 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+using HF.EventHorizon.Core.Entities;
+
 namespace HF.EventHorizon.Infrastructure.Data;
 
 public class EvtHorizonContext : DbContext
 {
+    public DbSet<ProtocolConnection> ProtocolConnections { get; set; }
+    public DbSet<DestinationMap> DestinationMaps { get; set; }
+    public DbSet<RoutingRule> RoutingRules { get; set; }
+    public DbSet<ProtocolPlugin> ProtocolPlugins { get; set; }
+    public DbSet<BrowsedAddress> BrowsedAddresses { get; set; }
+
     public EvtHorizonContext(DbContextOptions<EvtHorizonContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<ProtocolConnection>()
+            .HasIndex(p => p.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<DestinationMap>()
+            .HasOne(d => d.RoutingRule)
+            .WithMany(r => r.DestinationMaps)
+            .HasForeignKey(d => d.RoutingRuleId)
+            .OnDelete(DeleteBehavior.Restrict);
+
         base.OnModelCreating(modelBuilder);
     }
 }
---
+++ src/HF.EventHorizon.Infrastructure/Migrations/20241213175422_InitialCreate.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HF.EventHorizon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProtocolPlugins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PluginDirectoryPath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PluginTypesCsv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RequiredParametersCsv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProtocolPlugins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProtocolConnections",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProtocolPluginId = table.Column<int>(type: "int", nullable: false),
+                    ConnectionString = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AdditionalParametersJson = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProtocolConnections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProtocolConnections_ProtocolPlugins_ProtocolPluginId",
+                        column: x => x.ProtocolPluginId,
+                        principalTable: "ProtocolPlugins",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BrowsedAddresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProtocolConnectionId = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrowsedAddresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BrowsedAddresses_ProtocolConnections_ProtocolConnectionId",
+                        column: x => x.ProtocolConnectionId,
+                        principalTable: "ProtocolConnections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoutingRules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProtocolConnectionId = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoutingRules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoutingRules_ProtocolConnections_ProtocolConnectionId",
+                        column: x => x.ProtocolConnectionId,
+                        principalTable: "ProtocolConnections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DestinationMaps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProtocolConnectionId = table.Column<int>(type: "int", nullable: false),
+                    RoutingRuleId = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DestinationMaps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DestinationMaps_ProtocolConnections_ProtocolConnectionId",
+                        column: x => x.ProtocolConnectionId,
+                        principalTable: "ProtocolConnections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DestinationMaps_RoutingRules_RoutingRuleId",
+                        column: x => x.RoutingRuleId,
+                        principalTable: "RoutingRules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrowsedAddresses_ProtocolConnectionId",
+                table: "BrowsedAddresses",
+                column: "ProtocolConnectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DestinationMaps_ProtocolConnectionId",
+                table: "DestinationMaps",
+                column: "ProtocolConnectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DestinationMaps_RoutingRuleId",
+                table: "DestinationMaps",
+                column: "RoutingRuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProtocolConnections_Name",
+                table: "ProtocolConnections",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProtocolConnections_ProtocolPluginId",
+                table: "ProtocolConnections",
+                column: "ProtocolPluginId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoutingRules_ProtocolConnectionId",
+                table: "RoutingRules",
+                column: "ProtocolConnectionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BrowsedAddresses");
+
+            migrationBuilder.DropTable(
+                name: "DestinationMaps");
+
+            migrationBuilder.DropTable(
+                name: "RoutingRules");
+
+            migrationBuilder.DropTable(
+                name: "ProtocolConnections");
+
+            migrationBuilder.DropTable(
+                name: "ProtocolPlugins");
+        }
+    }
+}
